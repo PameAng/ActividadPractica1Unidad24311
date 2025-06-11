0db27ef32f102d380129dd2d56bb123e05a5d751
PameAng <pamelaangomas@hotmail.com>
2025-06-11 15:06:51 -0300

    Arreglos ortográficos

--- Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(225, 32);
             this.label1.TabIndex = 1;
-            this.label1.Text = "El texto cambiara";
+            this.label1.Text = "El texto cambiará";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
